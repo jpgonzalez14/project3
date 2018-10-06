a57095f68d23d491209380de4c441d6132b88897
Juan Pablo Gonzalez <jp.gonzalez14@uniandes.edu.co>
2018-10-06 17:42:12 -0500

    footer

--- imports/ui/layout/Footer.js
+++ imports/ui/layout/Footer.js
@@ -2,7 +2,65 @@ import React from 'react';
 
 class Footer extends React.Component{
   render(){
-    return <p>Footer</p>;
+    return (
+
+<footer className="page-footer font-small special-color-dark pt-4">
+
+    <div className="container">
+
+      <ul className="list-unstyled list-inline text-center">
+        <li className="list-inline-item">
+          <a className="btn-floating btn-fb mx-1">
+            <i className="fa fa-facebook"> </i>
+          </a>
+        </li>
+        <li className="list-inline-item">
+          <a className="btn-floating btn-tw mx-1">
+            <i className="fa fa-twitter"> </i>
+          </a>
+        </li>
+        <li className="list-inline-item">
+          <a className="btn-floating btn-gplus mx-1">
+            <i className="fa fa-google-plus"> </i>
+          </a>
+        </li>
+        <li className="list-inline-item">
+          <a className="btn-floating btn-li mx-1">
+            <i className="fa fa-linkedin"> </i>
+          </a>
+        </li>
+        <li className="list-inline-item">
+          <a className="btn-floating btn-dribbble mx-1">
+            <i className="fa fa-dribbble"> </i>
+          </a>
+        </li>
+      </ul>
+
+    </div>
+
+    <div className="footer-copyright text-center py-3">Created by:
+          <a
+            className="text-primary-color"
+            href="http://juanpgonzalez.com/"
+            target="_blank"
+          >
+            {' '}
+            Juan Pablo{' '}
+          </a>{' '}
+          &
+          <a
+            className="text-primary-color"
+            href="http://rangel10.github.io/"
+            target="_blank"
+          >
+            {' '}
+            Ricardo Angel{' '}
+          </a>{' '}
+    </div>
+
+  </footer>
+
+    );
   }
 }
 
